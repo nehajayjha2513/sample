de9f46a6a90f5d5ab69cdb1e04c54925fd366fb7
nehajayjha2513 <46707655+nehajayjha2513@users.noreply.github.com>
2019-02-13 22:58:51 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.nit.ora;
+
+/**
+ * @author hi
+ *
+ */
+public class hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		// TODO Auto-generated method stub
+
+	}
+
+}
